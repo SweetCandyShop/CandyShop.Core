4c3e86895a9e209a125183556a7e1cd065e44893
Yan Magdych <magdychyan@gmail.com>
2021-08-18 21:59:30 +0300

    Update Database with new Sweetness proce field

---
+++ src/CandyShop.Infrastructure/Migrations/20210818185812_AddFieldPrice.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandyShop.Infrastructure.Migrations
+{
+    public partial class AddFieldPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Sweetnesses",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Sweetnesses");
+        }
+    }
+}
