643bce9ba2bf88f35aa8117421985a9f7b020c91
Yan Magdych <magdychyan@gmail.com>
2021-08-19 00:21:53 +0300

    Update database with users

--- src/CandyShop.Infrastructure/Migrations/20210818212032_AddUsers.Designer.cs
+++ src/CandyShop.Infrastructure/Migrations/20210818212032_AddUsers.Designer.cs
@@ -3,14 +3,16 @@ using CandyShop.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandyShop.Infrastructure.Migrations
 {
     [DbContext(typeof(CandyShopDbContext))]
-    partial class CandyShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210818212032_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
