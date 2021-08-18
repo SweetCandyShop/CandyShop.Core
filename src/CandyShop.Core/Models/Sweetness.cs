f3c372608ec8fe745a5f548ca5a7281324e03e4b
Yan Magdych <magdychyan@gmail.com>
2021-08-18 21:58:58 +0300

    Add filed Price

--- src/CandyShop.Core/Models/Sweetness.cs
+++ src/CandyShop.Core/Models/Sweetness.cs
@@ -5,6 +5,7 @@ namespace CandyShop.Core.Models
     public sealed class Sweetness : DataBaseUnit, IEntity
     {
         public string Name{ get; set; }
+        public float Price { get; set; }
         public float Weight { get; set; }
         public int Calories { get; set; }
         public string ShelfLife { get; set; }
