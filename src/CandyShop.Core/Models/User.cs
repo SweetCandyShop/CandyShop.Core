e2232060f5ba812c9f2fad035d6c92d5b4d0c1f2
Yan Magdych <magdychyan@gmail.com>
2021-08-19 00:21:06 +0300

    Add user model

---
+++ src/CandyShop.Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop.Core.Models
+{
+    public sealed class User : DataBaseUnit, IEntity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
